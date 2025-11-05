8595bb32e52934ad7d686e96dc883780c5169ec1
Victory-Hugo <2023110098@stu.cqmu.edu.cn>
2025-11-05 18:10:29 +0800

    2025年11月5日

--- 7-单倍群分型/1-HaploCart/src/2-分型.sh
+++ 7-单倍群分型/1-HaploCart/src/2-分型.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 
-INPUT_FILE="/mnt/f/OneDrive/文档（科研）/脚本/Download/10-mtDNA/7-单倍群分型/1-HaploCart/data/rCRS.fa.gz"
-sample_prefix="rCRS"
+INPUT_FILE="/mnt/f/OneDrive/文档（科研）/脚本/Download/10-mtDNA/7-单倍群分型/1-HaploCart/example/1.fa"
+sample_prefix="1"
 OUTPUT_FILE="/mnt/f/OneDrive/文档（科研）/脚本/Download/10-mtDNA/7-单倍群分型/1-HaploCart/output/HaploCart"
 HC_FILE_DIR="/mnt/f/OneDrive/文档（科研）/脚本/Download/10-mtDNA/7-单倍群分型/1-HaploCart/data"
 posterior_file="/mnt/f/OneDrive/文档（科研）/脚本/Download/10-mtDNA/7-单倍群分型/1-HaploCart/output/HaploCart_posterior"
